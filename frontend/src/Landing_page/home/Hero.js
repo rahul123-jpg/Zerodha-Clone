d6d7c081a6ec91d5d04480a7e9b0b0f0fad92fec
Rahul Singh <rb9238874@gmail.com>
2025-11-24 16:39:12 +0530

    Update Hero.js

--- frontend/src/Landing_page/home/Hero.js
+++ frontend/src/Landing_page/home/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
@@ -8,7 +9,13 @@ function Hero() {
         <h1 className='mt-5 '>
           Invest in everything</h1>
         <p>Online platform to invest in stocks ,derivates,mutual fund,and more</p>
-        <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Sign Up Now</button>
+          <Link 
+          to="/signup" 
+          className='p-3 btn btn-primary fs-5 mb-5' 
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Sign Up Now
+        </Link>
       </div>
     </div>
   )
